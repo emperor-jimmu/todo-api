356e06688f59146bef3ee56024f769c321adf59c
Nimrod Perez <nimrodperez@Nimrods-MacBook-Pro.local>
2017-11-09 16:34:58 +0200

    Added more validation and writing to Users collection

--- server/server.js
+++ server/server.js
@@ -7,24 +7,45 @@ mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', {
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 2
     },
     completed: {
+        default: false,
         type: Boolean
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 var newTodo = new Todo({
-    text: 'Eat noodles',
-    completed: true,
+    text: '   Blast asteroids',
     completedAt: 1913330229
 });
+
+var User = mongoose.model("User", {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+})
 
 newTodo.save().then((doc) => {
     console.log('Saved -', JSON.stringify(doc, undefined, 2));
 }).catch((e) => {
     console.log('Unable to save todo');
+});
+
+var newUser = new User({
+    email: 'drdre@drefamily.com '
+}).save().then((doc) => {
+    console.log('Saved - ', JSON.stringify(doc, undefined, 2));
+}).catch((e) => {
+    console.log('Unable to save user');
 });
